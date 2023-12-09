a2da1b40cd91c1b110a0ce7afdbe41cc681ef62b
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-12-09 16:28:03 +0000

    Deploying to gh-pages from @ 01eksandr0/goit-js-hw-10@3f6c819f53d998e7d7df56d0efe706625c7b81df 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */const m=(e,i)=>new Promise((t,o)=>{setTimeout(()=>{i?t(e):o(e)},e)}).then(t=>{iziToast.success({title:"OK",message:`✅ Fulfilled promise in ${t}ms`,position:"topCenter"})}).catch(t=>{iziToast.error({message:`❌ Rejected promise in ${t}ms`,position:"center"})}),s=document.querySelector(".form");let r=!0;s.addEventListener("submit",e=>{e.preventDefault(),s.state.value==="rejected"&&(r=!1),m(s.delay.value,r),s.reset(),r=!0});
+//# sourceMappingURL=commonHelpers3.js.map
